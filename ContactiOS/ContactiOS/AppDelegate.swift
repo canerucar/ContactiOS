8247374655381099432f55ebfa0dea8831a8b1e1
Caner Uçar <canerucar0@gmail.com>
2020-01-05 00:38:28 +0300

    Project created.

---
+++ ContactiOS/ContactiOS/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ContactiOS
+//
+//  Created by Caner Uçar on 5.01.2020.
+//  Copyright © 2020 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ ContactiOS/ContactiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactiOS
+//
+//  Created by Caner Uçar on 5.01.2020.
+//  Copyright © 2020 Caner Uçar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
